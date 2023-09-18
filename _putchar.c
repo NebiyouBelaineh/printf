e8d955ed39db7767a2b55d55355a8d84e86265b0
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-18 12:32:13 +0300

    Add _putchar function

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _putchar - prints character passed to it
+ * @c: character argument to be printed
+ * Return: 0 always
+ */
+int _putchar(char c)
+{
+	putchar(c);
+	return (0);
+}
