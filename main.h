6d61d6acf791bd09e40ca3eec99fb367e4b445a1
Bruka25 <bkteshome862@gmail.com>
2023-09-18 12:29:23 +0300

    fix printf header

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 
-int _printf(consit char *format, ...);
+int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list print, const char *format);
 
