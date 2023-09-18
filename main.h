a220bb9407fd5db6243993c8069c201cf32aa987
Bruka25 <bkteshome862@gmail.com>
2023-09-18 11:42:20 +0300

    main.h

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+int _printf(consit char *format, ...);
+int _putchar(char c);
+
+#endif/*MAIN_H*/
