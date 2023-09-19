f8ebbda5ce4b70f038a9eae05d8dba76893995b4
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-19 16:41:12 +0300

    Add function to select functions

--- get_func.c
+++ get_func.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
 /**
- * get_func - gets the right function to use depending on the
- * conversion specifier
- *
- * @specifier: convsersion specifier passed to get_func
- *
- * Return: appropriate function along with respective return value.
+* get_func - gets the right function and calls a respective function
+* to print arguments depending on conversion specifier
+*
+* @format: current format to be checked
+* @func_print: Struct holding appropriate function for conversion
+* @buffer: Buffer needed to copy
+* @buf_lenptr: Pointer to the length of the buffer
+* @buf_posptr: Pointer to the position within the buffer
+* @print: va_list to retrieve all arguments to _printf function
+*
+* Return: Return the number of bytes copied
+*
 */
 
-int (*get_func(char specifier))(va_list print)
+int get_func(char format, prt_func func_print[], char buffer[],
+		     int *buf_lenptr, int *buf_posptr, va_list print)
 {
-	prt_func func_print[] = {
-		{'i', _print_int},
-		{'s', print_string},
-		{'c', print_char},
-		{'d', _print_int},
-		{'%', print_percent}
-	};
-	int i, size = 5;
+	int i, bytes, len = 4;
 
-	for (i = 0; i < size; i++)
-		if (func_print[i].symbol == specifier)
-			return (func_print[i].fun_pt);
-
-	return (NULL);
+	bytes = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (format == func_print[i].symbol)
+		{
+			bytes += func_print[i].fun_pt(print, buffer, buf_lenptr,
+						  buf_posptr);
+			return (bytes);
+		}
+	}
+	return (0);
 }
